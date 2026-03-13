ad8d823460a725fd005988288f4dfe4a92f5af9a
Oshawhat <alannobledall@gmail.com>
2026-03-13 14:42:20 -0500

    allow falling through platforms by holding down

--- player.gd
+++ player.gd
@@ -6,6 +6,9 @@ const SPEED = 500
 const ACCELERATION = 2500
 const JUMP_STRENGTH = 1200
 
+var normal_mask = 0b00000011
+var fall_mask =   0b00000001
+
 
 func _ready() -> void:
 	pass
@@ -20,5 +23,7 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity()
 	elif Input.is_action_just_pressed("move_up"):
 		velocity.y -= JUMP_STRENGTH
+	
+	collision_mask = normal_mask if !Input.is_action_pressed("move_down") else fall_mask
 	
 	move_and_slide()
