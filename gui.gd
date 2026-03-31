bda877630f33bb32f2d58ad0c3a6bb78358dc640
Oshawhat <alannobledall@gmail.com>
2026-03-31 14:15:54 -0500

    also do stuff

--- gui.gd
+++ gui.gd
@@ -1,13 +1,17 @@
+class_name GUI
 extends Control
 
 
 const lives_text = "     x%d"
 
-@export var player: Player
-@export var enemy: Enemy
+
+@onready var level_1_button: Button = $CanvasLayer/Panel2/Level1Button
+@onready var level_2_button: Button = $CanvasLayer/Panel2/Level2Button
+@onready var level_3_button: Button = $CanvasLayer/Panel2/Level3Button
+@onready var main_menu: Panel = $CanvasLayer/Panel2
 
 
-func _process(_delta: float) -> void:
+func update(player: Player, enemy: Enemy) -> void:
 	$CanvasLayer/Panel/PlayerDisplay/VBoxContainer/HBoxContainer/VBoxContainer/PlayerDamage.text = str(player.damage_taken) + "%"
 	$CanvasLayer/Panel/EnemyDisplay/VBoxContainer/HBoxContainer/VBoxContainer/EnemyDamage.text = str(enemy.damage_taken) + "%"
 	$CanvasLayer/Panel/PlayerDisplay/VBoxContainer/HBoxContainer/VBoxContainer/PlayerLives.text = lives_text % player.lives
--- main.gd
+++ main.gd
@@ -9,21 +9,44 @@ enum Arenas {
 
 ## The arena that is currently in play
 @export var current_arena: Arena
+@export var current_player: Player
+@export var current_enemy: Enemy
 
 @export var prehistoric_arena: PackedScene = preload("res://prehistoric_arena.tscn")
 @export var space_arena: PackedScene = preload("res://space_arena.tscn")
 
-@onready var player: Player = get_node("Player")
-@onready var enemy: Enemy = get_node("Enemy")
+@export var min_swap_time: float = 20
+@export var max_swap_time: float = 40
+
+@onready var player: PackedScene = preload("res://player.tscn")
+@onready var enemy: PackedScene = preload("res://paytenemy.tscn")
+
+@onready var gui: GUI = $GUI
+
+var playing: bool = false
+var swap_timer: float = randf_range(min_swap_time, max_swap_time)
+var next_arena: Arenas = Arenas.SPACE
 
 
 func _ready() -> void:
-	$AudioStreamPlayer.stream = current_arena.music
-	$AudioStreamPlayer.play()
+	gui.level_1_button.pressed.connect(start_level.bind(1))
+	gui.level_2_button.pressed.connect(start_level.bind(2))
+	gui.level_3_button.pressed.connect(start_level.bind(3))
+
+
+func _process(delta: float) -> void:
+	if not playing:
+		return
+	$GUI.update(player, enemy)
+	swap_timer -= delta
+	if swap_timer <= 0:
+		set_arena(next_arena)
+		next_arena = Arenas.PREHISTORIC if next_arena == Arenas.SPACE else Arenas.SPACE
 
 
 func set_arena(new_arena: Arenas) -> void:
-	current_arena.queue_free()
+	if current_arena:
+		current_arena.queue_free()
 	var arena_inst
 	
 	match new_arena:
@@ -35,8 +58,28 @@ func set_arena(new_arena: Arenas) -> void:
 	add_child(arena_inst)
 	current_arena = arena_inst
 	
-	player.position.y = -350
-	enemy.position.y = -350
+	$AudioStreamPlayer.stream = current_arena.music
+	$AudioStreamPlayer.play()
+	
+	current_player.position.y = -350
+	current_enemy.position.y = -350
+
+
+func start_level(level_num: int):
+	gui.main_menu.hide()
+	
+	current_player = player.instantiate()
+	add_child(current_player)
+	current_player.position = $PlayerSpawn.position
+	
+	current_enemy = enemy.instantiate()
+	add_child(current_enemy)
+	current_enemy.position = $EnemySpawn.position
+	
+	set_arena(Arenas.PREHISTORIC)
+	next_arena = Arenas.SPACE
+
 
 func level_over() -> void:
-	pass
+	current_player.process_mode = Node.PROCESS_MODE_DISABLED
+	current_enemy.process_mode = Node.PROCESS_MODE_DISABLED
