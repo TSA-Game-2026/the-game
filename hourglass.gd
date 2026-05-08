5bc1d9e12de3faa561cfbce9b2b393bf2c0c62bd
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 <pcpuglife313@gmail.com>
2026-05-08 09:28:35 -0500

    add hourglass logic

---
+++ hourglass.gd
@@ -0,0 +1,18 @@
+extends PanelContainer
+
+
+@onready var main: Main = self.get_tree().current_scene
+
+
+func _process(delta: float) -> void:
+	$CenterContainer/AnimatedSprite2D.speed_scale = 5 if main.swap_timer <= 5 else 1
+
+
+func flip():
+	var flip_tween = create_tween().set_ignore_time_scale()
+	flip_tween.tween_property($Control, "rotation", 180, 1)
+	
+	await flip_tween.finished
+	
+	$Control/CenterContainer/AnimatedSprite2D.play()
+	$Control.rotation = 0
