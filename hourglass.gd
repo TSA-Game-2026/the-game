84aad495990882bdcfbd40ab09f656363dff98e6
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 <pcpuglife313@gmail.com>
2026-05-12 08:37:15 -0500

    hourglass stuff

--- hourglass.gd
+++ hourglass.gd
@@ -1,26 +1,35 @@
-extends PanelContainer
+extends Panel
 
 
 @onready var main: Main = self.get_tree().current_scene
 
 
 func _ready() -> void:
-	$Control/CenterContainer/AnimatedSprite2D.play()
+	$Control/CenterContainer/Control/AnimatedSprite2D.play()
 
 
 func _process(delta: float) -> void:
-	$CenterContainer/AnimatedSprite2D.speed_scale = 5 if main.swap_timer <= 5 else 1
+	$Control/CenterContainer/Control/AnimatedSprite2D.speed_scale = 5 if main.swap_timer <= 5 else 1
 
 
 func flip():
+	var sprite = $Control/CenterContainer/Control/AnimatedSprite2D
+	sprite.play()
+	
+	await sprite.animation_finished
+	
 	var flip_tween = create_tween().set_ignore_time_scale()
-	flip_tween.tween_property($Control, "rotation", 180, 1)
+	flip_tween.tween_property($Control, "rotation_degrees", 180, 1)
 	
 	await flip_tween.finished
 	
-	$Control/CenterContainer/AnimatedSprite2D.play()
-	$Control.rotation = 0
+	sprite.play_backwards()
+	
+	await sprite.animation_finished
+	
+	var flip_tween_2 = create_tween().set_ignore_time_scale()
+	flip_tween.tween_property($Control, "rotation_degrees", 0, 1)
+	
+	await flip_tween_2.finished
 
-
-func _on_animated_sprite_2d_animation_finished() -> void:
 	flip()
